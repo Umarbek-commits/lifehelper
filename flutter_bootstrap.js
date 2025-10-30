bd22e8b54b7afe0baa26253dc6d801a542816af7
Umarbek <sidikovoatillo44@gmail.com>
2025-10-30 22:07:52 +0600

    Deploy fixed base href to gh-pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "115908897"
+    serviceWorkerVersion: "1267529767"
   }
 });
